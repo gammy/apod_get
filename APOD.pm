6d38e800fd6eea72742cb3f75fad7e0b00956438
gammy <gammy@users.noreply.github.com>
2018-10-02 12:54:54 +0200

    Update indentation

--- APOD.pm
+++ APOD.pm
@@ -24,7 +24,7 @@ use strict;
  $apod->destination("/tmp/");
 
  if(-e $apod->destination . '/' . $apod->filename) {
-	 die "We already have the APOD!\n";
+     die "We already have the APOD!\n";
  }
  
  $apod->save_image();
@@ -67,51 +67,51 @@ sub description { return $_[0]->{description}; }
 
 # Getter/setter
 sub destination { 
-	my $self = shift;
-	if(@_) {
-		$self->{destination} = shift;
-	} else {
-		if(! $self->{destination}) {
-			$self->{destination} = '.';
-		}
-	}
+    my $self = shift;
+    if(@_) {
+        $self->{destination} = shift;
+    } else {
+        if(! $self->{destination}) {
+            $self->{destination} = '.';
+        }
+    }
 
-	return $self->{destination};
+    return $self->{destination};
 };
 
 # Get the APOD webpage and find image url & description. 
 # Always call this first.
 # Takes optional page URL as argument
 sub peek {
-	my $self = shift;
+    my $self = shift;
 
-	$self->{description} = undef;
-	$self->{url}         = undef;
-	$self->{image}       = undef;
-	$self->{filename}    = undef;
-	$path_found          = 0;
+    $self->{description} = undef;
+    $self->{url}         = undef;
+    $self->{image}       = undef;
+    $self->{filename}    = undef;
+    $path_found          = 0;
 
-	if(@_) {
-		$self->{page_url} = shift;
-	} else {
-		$self->{page_url} = BASE_PAGE;
-	}
+    if(@_) {
+        $self->{page_url} = shift;
+    } else {
+        $self->{page_url} = BASE_PAGE;
+    }
 
     $ua = LWP::UserAgent->new();
     my $html;
-	my $ret = $ua->get($self->{page_url});
+    my $ret = $ua->get($self->{page_url});
     if($ret->is_success) {
         $html = $ret->decoded_content;
     } else {
-		croak "Failed to get \"" . $self->{page_url} . "\": " . $ret->status_line;
+        croak "Failed to get \"" . $self->{page_url} . "\": " . $ret->status_line;
     }
 
-	$self->parse($html);
+    $self->parse($html);
 }
 
 sub get_image {
-	my $self = shift;
-	my $url = $self->{url};
+    my $self = shift;
+    my $url = $self->{url};
 
     my $ret = $ua->get($url);
     if($ret->is_success) {
@@ -122,72 +122,72 @@ sub get_image {
 }
 
 sub save_image {
-	my $self = shift;
-	my $dst = $self->destination . '/' . $self->{filename};
+    my $self = shift;
+    my $dst = $self->destination . '/' . $self->{filename};
 
-	croak "Call peek() first!" if ! $self->{description};
+    croak "Call peek() first!" if ! $self->{description};
 
-	$self->get_image();
-	
-	# user-defined argument overrides the /entire/ path including 'destination'
-	if(@_) { 
-		$dst = shift;
-	}
+    $self->get_image();
+    
+    # user-defined argument overrides the /entire/ path including 'destination'
+    if(@_) { 
+        $dst = shift;
+    }
 
-	open F, '>', $dst or croak "Can't open \"$dst\": $!";
-	print F $self->{image};
-	close F;
+    open F, '>', $dst or croak "Can't open \"$dst\": $!";
+    print F $self->{image};
+    close F;
 }
 
 sub save_description {
-	my $self = shift;
-	my $dst = $self->destination . '/' . $self->{filename} . '.txt';
+    my $self = shift;
+    my $dst = $self->destination . '/' . $self->{filename} . '.txt';
 
-	croak "Call peek() first!" if ! $self->{description};
-	
-	# user-defined argument overrides the /entire/ path  including 'destination'
-	if(@_) { 
-		$dst = shift;
-	}
+    croak "Call peek() first!" if ! $self->{description};
+    
+    # user-defined argument overrides the /entire/ path  including 'destination'
+    if(@_) { 
+        $dst = shift;
+    }
 
-	open F, '>', $dst or croak "Can't open \"$dst\": $!";
-	print F $self->{description};
-	close F;
+    open F, '>', $dst or croak "Can't open \"$dst\": $!";
+    print F $self->{description};
+    close F;
 }
 
 # Overrides empty HTML::Parser sub
 sub start { 
-	my ($self, $tagname, $attr, $attrseq, $origtext) = @_;
+    my ($self, $tagname, $attr, $attrseq, $origtext) = @_;
 
-	if($tagname eq 'p') {
-		if($text_buf =~m/Explanation:/s) {
-			# The order of these is not random.
-			$text_buf =~s/\n+/\n/g;
-			$text_buf =~s/\n/ /g;
-			$text_buf =~s/ +/ /g;
-			$text_buf =~s/^ //g;
-			$text_buf =~s/ $//g;
-			$text_buf =~s/^Explanation: //;
-			$self->{description} = $text_buf . 
-			                       " ($_[0]->{page_url})";
-		}
-		$text_buf = '';
-	}elsif($tagname eq 'a') {
-		if(defined $attr->{href} && $attr->{href}=~m#(image/.*)#s) {
-			if (! $path_found) {
-				$self->{url}      = BASE_URL . '/' . $1;
-				$self->{filename} = substr($self->{url}, 
-						           rindex($self->{url}, '/') + 1);
-			}
-			$path_found = 1;
-		}
-	}
+    if($tagname eq 'p') {
+        if($text_buf =~m/Explanation:/s) {
+            # The order of these is not random.
+            $text_buf =~s/\n+/\n/g;
+            $text_buf =~s/\n/ /g;
+            $text_buf =~s/ +/ /g;
+            $text_buf =~s/^ //g;
+            $text_buf =~s/ $//g;
+            $text_buf =~s/^Explanation: //;
+            $self->{description} = $text_buf . 
+                                   " ($_[0]->{page_url})";
+        }
+        $text_buf = '';
+    }elsif($tagname eq 'a') {
+        if(defined $attr->{href} && $attr->{href}=~m#(image/.*)#s) {
+            if (! $path_found) {
+                $self->{url}      = BASE_URL . '/' . $1;
+                $self->{filename} = substr($self->{url}, 
+                                   rindex($self->{url}, '/') + 1);
+            }
+            $path_found = 1;
+        }
+    }
 }
 
 # Overrides empty HTML::Parser sub
 sub text {
-	my ($self, $text) = @_;
-	$text_buf .= $text;
+    my ($self, $text) = @_;
+    $text_buf .= $text;
 }
 
 1;
